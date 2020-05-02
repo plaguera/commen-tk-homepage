cd4e72a1f4a6dd85258fe22163ca56185b2177ab
Pedro Lagüera <laguerapedro@gmail.com>
2020-05-02 12:52:23 +0100

    Added copy to clipboard button for script

--- index.js
+++ index.js
@@ -74,9 +74,23 @@ function onPageSizeChanged(event) {
     document.getElementById("hljs-value-page-size").innerText = '\'' + event.target.value + '\'';
 }
 
+function onCopyClipboardClicked(event) {
+    event.target.innerHTML = '<svg aria-hidden="true" height="16" role="img" viewBox="0 0 14 16" width="14" style="display:inline-block;fill:#28a745;user-select:none;vertical-align:text-bottom"><path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path></svg>';
+    setTimeout(function () {
+        var range = document.createRange();
+        range.selectNode(document.getElementById('copyArea'));
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();
+        event.target.innerHTML = '<svg aria-hidden="true" height="16" role="img" viewBox="0 0 14 16" width="14" style="display:inline-block;fill:currentColor;user-select:none;vertical-align:text-bottom"><path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg>';
+    }, 1000);
+}
+
 document.getElementById("inputType").addEventListener('change', onTypeChanged);
 document.getElementById("inputUser").addEventListener('change', onUserChanged);
 document.getElementById("inputRepo").addEventListener('change', onRepoChanged);
 document.getElementById("inputIssue").addEventListener('change', onIssueChanged);
 document.getElementById("inputTheme").addEventListener('change', onThemeChanged);
 document.getElementById("inputPageSize").addEventListener('input', onPageSizeChanged);
+document.getElementById("btnCopyClipboard").addEventListener('click', onCopyClipboardClicked);
